b4cf07d6e9b29092730a1edb5aafc8883ceb312f
Karolina Pacheco <karolina_cris@hotmail.com>
2021-06-13 18:47:58 -0300

    change info on course cards

--- client/src/components/shop/ItemList.js
+++ client/src/components/shop/ItemList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import note from '../../img/notebook.jpg';
 
 const ItemList = () => (
     <div>
@@ -10,14 +9,13 @@ const ItemList = () => (
                 <div className="card">
                     <div className="card-image" >
                         <img alt="notebook" src={"https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"} />
-                        <span className="card-title">Hacker mal</span>
                     </div>
                     <div className="card-content">
-                        Aprenda a ser um hacker mal.
-                        <p> <a href="/">$ 12.90</a></p>
+                        Chatbots para Iniciantes
+                        <p> <a href="/">R$ 27.90</a></p>
                     </div>
                     <div className="card-action">
-                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/course/chatbots-python-dialogflow-iniciantes/">Comprar agora!</a>
                     </div>
                 </div>
 
@@ -28,14 +26,13 @@ const ItemList = () => (
                 <div className="card">
                     <div className="card-image" >
                         <img alt="notebook" src={"https://images.unsplash.com/photo-1454165205744-3b78555e5572?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fHRlY2hub2xvZ3l8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"} />
-                        <span className="card-title">Hacker mal</span>
                     </div>
                     <div className="card-content">
-                        Aprenda a ser um hacker mal.
-                        <p> <a href="/">$ 12.90</a></p>
+                        Machine Learning
+                        <p> <a href="/">Gratuito</a></p>
                     </div>
                     <div className="card-action">
-                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://pt.coursera.org/learn/machine-learning">Comprar agora!</a>
                     </div>
                 </div>
                 
