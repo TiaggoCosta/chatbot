00b38bed024ec5a657338610504d3f853a2da025
Tiago Costa <tiago.tagcosta@gmail.com>
2021-06-13 17:26:26 -0300

    cards on list

--- client/src/components/shop/ItemList.js
+++ client/src/components/shop/ItemList.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import note from '../../img/notebook.jpg';
 
 const ItemList = () => (
     <div>
-        <div className="card-panel grey lighten-5 z-depth-1">
+        <div className="card-panel grey row lighten-5 z-depth-1">
 
-            <div  style={{ height: 270, paddingRight:30, float: 'left'}}>
+            <div className="col s12 m6">
+
                 <div className="card">
-                    <div className="card-image" style={{ width: 240}}>
-                        <img alt="notebook" src={require("../../img/notebook.jpg")} />
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
                         <span className="card-title">Hacker mal</span>
                     </div>
                     <div className="card-content">
@@ -19,10 +21,9 @@ const ItemList = () => (
                     </div>
                 </div>
 
-                <div  style={{ height: 270, paddingRight:30, float: 'left'}}>
                 <div className="card">
-                    <div className="card-image" style={{ width: 240}}>
-                        <img alt="notebook" src={require("../../img/notebook.jpg")} />
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
                         <span className="card-title">Hacker mal</span>
                     </div>
                     <div className="card-content">
@@ -33,25 +34,37 @@ const ItemList = () => (
                         <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
                     </div>
                 </div>
+
+                <div className="card">
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
+                        <span className="card-title">Hacker mal</span>
+                    </div>
+                    <div className="card-content">
+                        Aprenda a ser um hacker mal.
+                        <p> <a href="/">$ 12.90</a></p>
+                    </div>
+                    <div className="card-action">
+                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
+                        <span className="card-title">Hacker mal</span>
+                    </div>
+                    <div className="card-content">
+                        Aprenda a ser um hacker mal.
+                        <p> <a href="/">$ 12.90</a></p>
+                    </div>
+                    <div className="card-action">
+                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                    </div>
+                </div>
+
             </div>
 
-            <div  style={{ height: 270, paddingRight:30, float: 'left'}}>
-                <div className="card">
-                    <div className="card-image" style={{ width: 240}}>
-                        <img alt="notebook" src={require("../../img/notebook.jpg")} />
-                        <span className="card-title">Hacker mal</span>
-                    </div>
-                    <div className="card-content">
-                        Aprenda a ser um hacker mal.
-                        <p> <a href="/">$ 12.90</a></p>
-                    </div>
-                    <div className="card-action">
-                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
-                    </div>
-                </div>
-            </div>
-            
-            </div>
         </div>
     </div>
 )
