59123619269505ea47b5b1830a450c0d196210c5
Tiago Costa <tiago.tagcosta@gmail.com>
2021-06-13 16:25:04 -0300

    change landing vh

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 const Landing = () => (
     <header className="header">
-            <div className="logo-box">
-                <img src={require("../../img/logo-white.png")} alt="Logo" className="logo"/>
-            </div>
-
+           
             <div className="text-box">
                 <h1 className="heading-primary">
                     <span className="heading-primary-main">Outdoors</span>
                     <span className="heading-primary-sub">is where life happens</span>
                 </h1>
-
-                <a href="#" className="btn btn-white btn-animated">Discover our tour</a>
 
             </div>
             
