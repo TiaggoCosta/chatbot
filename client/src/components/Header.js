c2d56fe5d4129daccd485d56ff68bdda577fb9b2
Karolina Pacheco <karolina_cris@hotmail.com>
2021-06-13 14:40:31 -0300

    more translations

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => (
     <nav>
         <div className="nav-wrapper">
-            <Link to={'/'} className="brand-logo">IT courses</Link>
+            <Link to={'/'} className="brand-logo">Cursos de TI</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to={'/shop'}>Shop</Link></li>
-                <li><Link to={'/about'}>About us</Link></li>
+                <li><Link to={'/about'}>Sobre</Link></li>
             </ul>
         </div>
     </nav>
