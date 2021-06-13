5307108cdd9aa961c89fadc6cd7dba46907bc4a3
Tiago Costa <tiago.tagcosta@gmail.com>
2021-06-13 17:53:56 -0300

    fix row cards

--- client/src/components/shop/ItemList.js
+++ client/src/components/shop/ItemList.js
@@ -3,23 +3,9 @@ import note from '../../img/notebook.jpg';
 
 const ItemList = () => (
     <div>
-        <div className="card-panel grey row lighten-5 z-depth-1">
+        <div className="card-panel grey lighten-5 z-depth-1 row">
 
-            <div className="col s12 m6">
-
-                <div className="card">
-                    <div className="card-image" >
-                        <img alt="notebook" src={note} />
-                        <span className="card-title">Hacker mal</span>
-                    </div>
-                    <div className="card-content">
-                        Aprenda a ser um hacker mal.
-                        <p> <a href="/">$ 12.90</a></p>
-                    </div>
-                    <div className="card-action">
-                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
-                    </div>
-                </div>
+            <div className="col m6 " style={{float: 'left'}}>
 
                 <div className="card">
                     <div className="card-image" >
@@ -35,19 +21,9 @@ const ItemList = () => (
                     </div>
                 </div>
 
-                <div className="card">
-                    <div className="card-image" >
-                        <img alt="notebook" src={note} />
-                        <span className="card-title">Hacker mal</span>
-                    </div>
-                    <div className="card-content">
-                        Aprenda a ser um hacker mal.
-                        <p> <a href="/">$ 12.90</a></p>
-                    </div>
-                    <div className="card-action">
-                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
-                    </div>
-                </div>
+            </div>
+
+            <div className="col m6 " style={{float: 'left'}}>
 
                 <div className="card">
                     <div className="card-image" >
@@ -62,7 +38,43 @@ const ItemList = () => (
                         <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
                     </div>
                 </div>
+                
+            </div>
 
+            <div className="col m6 " style={{float: 'left'}}>
+
+                <div className="card">
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
+                        <span className="card-title">Hacker mal</span>
+                    </div>
+                    <div className="card-content">
+                        Aprenda a ser um hacker mal.
+                        <p> <a href="/">$ 12.90</a></p>
+                    </div>
+                    <div className="card-action">
+                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className="col m6 " style={{float: 'left'}}>
+
+                <div className="card">
+                    <div className="card-image" >
+                        <img alt="notebook" src={note} />
+                        <span className="card-title">Hacker mal</span>
+                    </div>
+                    <div className="card-content">
+                        Aprenda a ser um hacker mal.
+                        <p> <a href="/">$ 12.90</a></p>
+                    </div>
+                    <div className="card-action">
+                        <a target="_blank" rel="noopener noreferrer" href="#">Comprar agora!</a>
+                    </div>
+                </div>
+                
             </div>
 
         </div>
